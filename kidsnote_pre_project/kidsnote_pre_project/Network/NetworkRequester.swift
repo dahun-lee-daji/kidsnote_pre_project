1cdcbb6c07acf1a3805f860e7508234066e1560f
dahun <ssss6596@gmail.com>
2024-08-29 18:49:36 +0900

    feat: NetworkRequester의 내부 동작 구현

--- kidsnote_pre_project/kidsnote_pre_project/Network/NetworkRequester.swift
+++ kidsnote_pre_project/kidsnote_pre_project/Network/NetworkRequester.swift
@@ -9,12 +9,50 @@ import Foundation
 
 final class NetworkRequester {
     
-    private let urlSession: URLSession
-    
-    init(configuration: URLSessionConfiguration = .defaultConfiguration) {
-        self.urlSession = URLSession(configuration: configuration)
+    enum NetworkRequesterError: Error {
+        case invalidStatusCode
+        case failTransformHTTPURLResponse
+        case failDecoding
+        case failPrettyPrint
     }
     
+    private let session: URLSession
+    
+    init(configuration: URLSessionConfiguration = .defaultConfiguration) {
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    func request<T: Decodable>(urlRequest: URLRequest) async throws -> T {
+        let (data, response) = try await session.data(for: urlRequest)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkRequesterError.failTransformHTTPURLResponse
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else { throw NetworkRequesterError.invalidStatusCode }
+        
+        return try decode(data)
+    }
+    
+    private func decode<T: Decodable>(_ data: Data) throws -> T {
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            prettyPrintedData(data)
+            throw NetworkRequesterError.failDecoding
+        }
+    }
+    
+    private func prettyPrintedData(_ data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+            let prettyData = try JSONSerialization.data(withJSONObject: jsonObject, options: [.prettyPrinted])
+            guard let prettyString = String(data: prettyData, encoding: .utf8)
+            else { throw NetworkRequesterError.failPrettyPrint }
+            print(prettyString)
+        } catch {
+            print("Failed to convert data to pretty-printed JSON: \(error.localizedDescription)")
+        }
+    }
 }
 
 private extension URLSessionConfiguration {
