1cd927947683f003fba0799c20b85e8ecba3cd22
dahun <ssss6596@gmail.com>
2024-08-29 18:16:30 +0900

    feat: URLRequest를 생성하는 책임을 갖는 EndPoint 정의

---
+++ kidsnote_pre_project/kidsnote_pre_project/Network/EndPoint.swift
@@ -0,0 +1,66 @@
+//
+//  EndPoint.swift
+//  kidsnote_pre_project
+//
+//  Created by 이다훈 on 8/29/24.
+//
+
+import Foundation
+
+fileprivate enum EndPointError: Error {
+    case failCreateURL
+    case failTransForm
+}
+
+protocol EndPointProducable {
+    var scheme: SchemeType { get }
+    var host: String { get }
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameter: [String: String]? { get }
+}
+
+extension EndPointProducable {
+    func asURLRequest() throws -> URLRequest {
+        var component = URLComponents()
+        component.scheme = scheme.rawValue
+        component.host = host
+        component.path = path
+        component.queryItems = .init(parameter: parameter)
+
+        guard let url = component.url else { throw EndPointError.failCreateURL }
+          
+        var request = URLRequest.init(url: url)
+        request.httpMethod = method.rawValue
+        return request
+    }
+}
+
+private extension Array<URLQueryItem> {
+    init?(parameter: [String: String?]?) {
+        if let parameter {
+            self = parameter.map { (key: String, value: String?) in
+                URLQueryItem(name: key, value: value)
+            }
+        } else {
+            return nil
+        }
+    }
+}
+
+enum SchemeType: String {
+    case http
+    case https
+}
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+struct EndPoint: EndPointProducable {
+    var scheme: SchemeType
+    var host: String
+    var method: HTTPMethod
+    var path: String
+    var parameter: [String : String]?
+}
